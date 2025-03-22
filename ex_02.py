fa0240104d6562ebd0026eb6f868ccbbf3275095
unknown <pf95fabio@gmail.com>
2025-03-22 15:48:51 -0300

    aceitar uma função com certo formato e corrigindo a formula

--- ex_02.py
+++ ex_02.py
@@ -3,19 +3,28 @@
     2𝑥ˆ2 + 2𝑥 − 6
     -b +- sqrt(b²-4ac) / 2a"""
 
-import matplotlib.pyplot as plt
 import numpy as np
 
+def resolve(a,b,c):
+    delta = b**2 - 4*a*c
+    if delta<0:
+        print("essa função não tem raízes")
+        return None
+    if delta == 0:
+        print(f"a raíz unica desa função vale {-b/(2*a):.2f}")
 
-def resolve():
-    x1 = (-2 + (2**2 - 4*2*-6)**0.5) / 2*2
-    x2 = (-2 + (2**2 + 4*2*-6)**0.5) / 2*2
+    x1 = (-b + (delta)**0.5) / (a*2)
+    x2 = (-b - (delta)**0.5) / (a*2)
+    print(f"os valores das raízes são {x1:.2f} e {x2:.2f}")
     return x1,x2
 
-delta = 2**2 - 4*2*-6
+funcao = input("digite a função no formato Ax²+Bx+C:\n")
 
-if delta >= 0:
-    print(delta)
-    x1, x2 = resolve()
-    x2 = np.real(x2)
-    print(f"os valores das raízes são {x1:.2f} e {x2:.2f}")
+a,b,c = funcao.replace('²', '').replace('+', '').split('x')
+a = int(a)
+b = int(b)
+c = int(c)
+print(a,b,c)
+
+
+x1, x2 = resolve(a,b,c)
