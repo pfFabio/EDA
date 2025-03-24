c30a40e25d85e8a347ada84412344064c6bdca85
unknown <pf95fabio@gmail.com>
2025-03-24 16:36:25 -0300

    removendo a soma e trocando o print para mostrar a interseção

--- ex_01.py
+++ ex_01.py
@@ -8,36 +8,29 @@ import matplotlib.pyplot as plt
 def resolve(x):
     y1 = 0.5 * x + 0.5
     y2 = x * -1 + 2
-    y3 = (1.5*x +2.5)/2
-    return y1,y2,y3
+    return y1,y2
     
 def cria_vetores(max_X):
     vals_x = []
     vals_y1 = []
     vals_y2 = []
-    vals_y3 = []
     for i in range(max_X):
-        y1, y2, y3 = resolve(i)
+        y1, y2 = resolve(i)
         vals_y1.append(y1)
         vals_y2.append(y2)
-        vals_y3.append(y3)
         vals_x.append(i)
-<<<<<<< HEAD
         if y1 == y2:
             plt.scatter(i,y1, color="black", label="interseção")
-=======
->>>>>>> 51aa469a0c2af86df59b1dd6fb76e7b2531c0f3e
-    return vals_x, vals_y1, vals_y2, vals_y3
+            print(f"o resultado do sistema de equações é: x = {i} e por consequencia y= {y1}")
+    return vals_x, vals_y1, vals_y2
     
-def des_linha(vals_x, vals_y1, vals_y2, vals_y3):
+def des_linha(vals_x, vals_y1, vals_y2):
     plt.plot(vals_x, vals_y1, color='blue', label='equação 1')
     plt.plot(vals_x, vals_y2, color='red', label='equação 2')
-    plt.plot(vals_x, vals_y3, color='green', label='soma')
     plt.legend()
     plt.show()
     
 while True:
     max_X = int(input("diga o valor maximo de X: "))
-    vals_x, vals_y1, vals_y2, vals_y3 = cria_vetores(max_X)      
-    des_linha(vals_x, vals_y1, vals_y2, vals_y3)
-    print("o resultado do sistema de equações é: y=(1.5*x + 2.5)/2")
+    vals_x, vals_y1, vals_y2 = cria_vetores(max_X)      
+    des_linha(vals_x, vals_y1, vals_y2)
